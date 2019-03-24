275aaa59a0e8004d0c6d11fd53f7963f5fe56da5
Hillary Stewart <hillarystewart4427@gmail.com>
2019-03-24 12:21:07 -0600

    Add setup for Redux and Router
    Co-authored-by: Whitney Burton <whitneyburton9@gmail.com>

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types'
+import { withRouter, Route, Switch } from 'react-router-dom';
+import { setProjects } from '../../actions'
+import { fetchProjects } from '../../thunks/fetchProjects'
 
-class App extends Component {
+
+export class App extends Component {
+  componentDidMount() {
+    this.props.fetchProjects()
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,5 +20,17 @@ class App extends Component {
   }
 }
 
-export default App;
 
+export const mapStateToProps = (state) => ({
+  projects: state.projects,
+  palettes: state.palettes,
+  error: state.error,
+  isLoading: state.isLoading
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+  setProjects: (projects) => dispatch(setProjects(projects)),
+  fetchProjects: () => dispatch(fetchProjects())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,9 +4,23 @@ import './styles/main.scss';
 import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
+import { rootReducer } from './reducers';
 
+const devTools = composeWithDevTools(applyMiddleware(thunk));
+const store = createStore(rootReducer, devTools);
+const provider = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(provider, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
