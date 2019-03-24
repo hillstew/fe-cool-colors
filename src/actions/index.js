d371372fd392feb0dada212e551fb4e09daf68a2
Hillary Stewart <hillarystewart4427@gmail.com>
2019-03-24 12:18:14 -0600

    Add actions for redux
    Co-authored-by: Whitney Burton <whitneyburton9@gmail.com>

---
+++ src/actions/index.js
@@ -0,0 +1,29 @@
+export const setColors = colors => ({
+  type: 'SET_COLORS',
+  colors
+});
+
+export const setLoading = bool => ({
+  type: 'SET_LOADING',
+  bool
+});
+
+export const setError = error => ({
+  type: 'SET_ERROR',
+  error
+});
+
+export const setProjects = projects => ({
+  type: 'SET_PROJECTS',
+  projects
+});
+
+export const setPalettes = palettes => ({
+  type: 'SET_PALETTES',
+  palettes
+});
+
+export const toggleLocked = color => ({
+  type: 'TOGGLE_LOCKED',
+  color
+});
