c225da1da7a12023b56c16941f166946f2056bdf
Whitney Burton <whitneyburton9@gmail.com>
2019-03-24 15:38:15 -0600

    Add ColorCard and toggleLocked function to lock button
    Co-authored-by: Hillary Stewart <hillarystewart4427@gmail.com>

---
+++ src/components/ColorCard/ColorCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const ColorCard = ({ color, toggleLocked }) => {
+  const { hex, isLocked, id } = color;
+
+  return (
+    <div className="ColorCard" style={{ backgroundColor: hex }}>
+      <p>{hex}</p>
+      <button
+        onClick={() => toggleLocked(id)}
+        className={isLocked ? 'locked-button' : 'unlocked-button'} />
+    </div>
+  );
+};
