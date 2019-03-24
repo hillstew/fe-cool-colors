ffdf4196a3f8565e3a7b2b6826640136e51de9b9
Whitney Burton <whitneyburton9@gmail.com>
2019-03-24 14:59:47 -0600

    Add fetchPalettes and setColors to App
    Co-authored-by: Hillary Stewart <hillarystewart4427@gmail.com>

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,36 +1,44 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import { withRouter, Route, Switch } from 'react-router-dom';
-import { setProjects } from '../../actions'
-import { fetchProjects } from '../../thunks/fetchProjects'
-
+import { setColors } from '../../actions';
+import { fetchProjects } from '../../thunks/fetchProjects';
+import { fetchPalettes } from '../../thunks/fetchPalettes';
+import { updateColors } from '../../utils/helper';
+import ColorContainer from '../ColorContainer/ColorContainer';
 
 export class App extends Component {
   componentDidMount() {
-    this.props.fetchProjects()
+    this.props.fetchProjects();
+    this.props.fetchPalettes();
+    this.props.setColors(updateColors());
   }
 
   render() {
     return (
       <div className="App">
         <h1>We're runnin!!!</h1>
+        <ColorContainer />
       </div>
     );
   }
 }
 
-
-export const mapStateToProps = (state) => ({
+export const mapStateToProps = state => ({
   projects: state.projects,
   palettes: state.palettes,
   error: state.error,
   isLoading: state.isLoading
-})
+});
 
-export const mapDispatchToProps = (dispatch) => ({
-  setProjects: (projects) => dispatch(setProjects(projects)),
-  fetchProjects: () => dispatch(fetchProjects())
-})
+export const mapDispatchToProps = dispatch => ({
+  setColors: colors => dispatch(setColors(colors)),
+  fetchProjects: () => dispatch(fetchProjects()),
+  fetchPalettes: () => dispatch(fetchPalettes())
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
