837ae04cb6312fe3c258faa0e43bb3077e31d140
Hillary Stewart <hillarystewart4427@gmail.com>
2019-03-25 16:59:34 -0600

    Add set project to projectsReducer and postData to api
    Co-authored-by: Whitney Burton <whitneyburton9@gmail.com>

--- src/reducers/projectsReducer.js
+++ src/reducers/projectsReducer.js
@@ -1,8 +1,10 @@
 export const projectsReducer = (state = [], action) => {
   switch (action.type) {
     case 'SET_PROJECTS':
-      return action.projects
+      return action.projects;
+    case 'SET_PROJECT':
+      return [...state, action.project];
     default:
       return state;
   }
-}
+};
--- src/utils/api.js
+++ src/utils/api.js
@@ -6,3 +6,18 @@ export const getData = async url => {
   }
   return await response;
 };
+
+export const postData = async (url, project) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(project),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  if (!response.ok) {
+    const error = await response.json();
+    throw new Error(error.message);
+  }
+  return await response;
+};
