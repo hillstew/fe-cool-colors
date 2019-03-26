801cb9a15adca424f324ee7428c4a5e2e8e8b7ff
Whitney Burton <whitneyburton9@gmail.com>
2019-03-26 17:39:42 -0600

    Add delete button to ProjectCard
    Co-authored-by: Hillary Stewart <hillarystewart4427@gmail.com>

--- src/containers/ProjectCard/ProjectCard.js
+++ src/containers/ProjectCard/ProjectCard.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
-import { PaletteCard } from '../../components/PaletteCard/PaletteCard';
 import { connect } from 'react-redux';
+import { PaletteCard } from '../../components/PaletteCard/PaletteCard';
 import { deletePalette } from '../../thunks/deletePalette';
+import { deleteProject } from '../../thunks/deleteProject';
+import { setColors } from '../../actions';
 
 export class ProjectCard extends Component {
   render() {
-    const { matchingPalettes, project, deletePalette, palettes } = this.props;
+    const {
+      matchingPalettes,
+      project,
+      deletePalette,
+      palettes,
+      deleteProject,
+      projects,
+      setColors
+    } = this.props;
     const paletteCards = matchingPalettes.map(palette => {
       return (
         <PaletteCard
           palette={palette}
           deletePalette={deletePalette}
           palettes={palettes}
+          setColors={setColors}
+          key={palette.id}
         />
       );
     });
     return (
-      <div className="ProjectCard">
-        <h3 className="project-name">{project.name}</h3>
-        <button className="project-edit-button" />
+      <div className='ProjectCard'>
+        <h3 className='project-name'>{project.name}</h3>
+        <button className='project-edit-button' />
+        <button
+          className='delete-project-button'
+          onClick={() => deleteProject(project.id, palettes, projects)}
+        />
         {paletteCards}
       </div>
     );
@@ -26,11 +42,15 @@ export class ProjectCard extends Component {
 }
 
 export const mapStateToProps = state => ({
-  palettes: state.palettes
+  palettes: state.palettes,
+  projects: state.projects
 });
 
 export const mapDispatchToProps = dispatch => ({
-  deletePalette: (id, palettes) => dispatch(deletePalette(id, palettes))
+  deletePalette: (id, palettes) => dispatch(deletePalette(id, palettes)),
+  deleteProject: (id, palettes, projects) =>
+    dispatch(deleteProject(id, palettes, projects)),
+  setColors: (colors) => dispatch(setColors(colors))
 });
 
 export default connect(
