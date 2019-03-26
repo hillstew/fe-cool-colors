3975f29238d80cd12049fde11388adf57c6b46d5
Whitney Burton <whitneyburton9@gmail.com>
2019-03-26 14:47:49 -0600

    Adjust project param name to data to make dynamic
    Co-authored-by: Hillary Stewart <hillarystewart4427@gmail.com>

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,10 +7,10 @@ export const getData = async url => {
   return await response;
 };
 
-export const postData = async (url, project) => {
+export const postData = async (url, data) => {
   const response = await fetch(url, {
     method: 'POST',
-    body: JSON.stringify(project),
+    body: JSON.stringify(data),
     headers: {
       'Content-Type': 'application/json'
     }
