3844cfb7ca356eca8540f859a182634721df04b7
Hillary Stewart <hillarystewart4427@gmail.com>
2019-03-26 16:35:33 -0600

    Add onClick to PaletteCard to delete palette
    Co-authored-by: Whitney Burton <whitneyburton9@gmail.com>

--- src/components/PaletteCard/PaletteCard.js
+++ src/components/PaletteCard/PaletteCard.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
-export const PaletteCard = ({ palette }) => {
-  const { name, color_1, color_2, color_3, color_4, color_5 } = palette;
+export const PaletteCard = ({ palette, deletePalette, palettes }) => {
+  const { name, color_1, color_2, color_3, color_4, color_5, id } = palette;
   return (
     <div className="PaletteCard">
-      <p>{name}</p>
-      <div className="palette">
-        <div style={{ backgroundColor: color_1 }} className="palette-box" />
-        <div style={{ backgroundColor: color_2 }} className="palette-box" />
-        <div style={{ backgroundColor: color_3 }} className="palette-box" />
-        <div style={{ backgroundColor: color_4 }} className="palette-box" />
-        <div style={{ backgroundColor: color_5 }} className="palette-box" />
+      <p className="palette-name">{name}</p>
+      <div className="palette-container">
+        <div className="palette">
+          <div style={{ backgroundColor: color_1 }} className="palette-box" />
+          <div style={{ backgroundColor: color_2 }} className="palette-box" />
+          <div style={{ backgroundColor: color_3 }} className="palette-box" />
+          <div style={{ backgroundColor: color_4 }} className="palette-box" />
+          <div style={{ backgroundColor: color_5 }} className="palette-box" />
+        </div>
+        <button
+          className="delete-palette-button"
+          onClick={() => deletePalette(id, palettes)}
+        />
       </div>
-      <button className="delete-palette-button">Delete</button>
     </div>
   );
 };
