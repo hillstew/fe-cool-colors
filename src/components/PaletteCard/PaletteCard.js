eb318225091d861e31e16f5cd2e4eaf219187d02
Whitney Burton <whitneyburton9@gmail.com>
2019-03-26 17:39:04 -0600

    Refactor PaletteCard return and add onClick to palette
    Co-authored-by: Hillary Stewart <hillarystewart4427@gmail.com>

--- src/components/PaletteCard/PaletteCard.js
+++ src/components/PaletteCard/PaletteCard.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import shortid from 'shortid';
 
-export const PaletteCard = ({ palette, deletePalette, palettes }) => {
+export const PaletteCard = (props) => {
+  const { palette, deletePalette, palettes, setColors } = props;
   const { name, color_1, color_2, color_3, color_4, color_5, id } = palette;
+  const colors = [color_1, color_2, color_3, color_4, color_5];
+  const colorPackages = colors.map(color => {
+    return { hex: color, isLocked: false, id: shortid.generate() };
+  });
+  const colorBoxes = colorPackages.map(color => {
+    return <div style={{ backgroundColor: color.hex }} className='palette-box' key={color.id} />;
+  });
+
   return (
-    <div className="PaletteCard">
-      <p className="palette-name">{name}</p>
-      <div className="palette-container">
-        <div className="palette">
-          <div style={{ backgroundColor: color_1 }} className="palette-box" />
-          <div style={{ backgroundColor: color_2 }} className="palette-box" />
-          <div style={{ backgroundColor: color_3 }} className="palette-box" />
-          <div style={{ backgroundColor: color_4 }} className="palette-box" />
-          <div style={{ backgroundColor: color_5 }} className="palette-box" />
+    <div className='PaletteCard'>
+      <p className='palette-name'>{name}</p>
+      <div className='palette-container'>
+        <div className='palette' onClick={() => setColors(colorPackages)}>
+          {colorBoxes}
         </div>
         <button
-          className="delete-palette-button"
+          className='delete-palette-button'
           onClick={() => deletePalette(id, palettes)}
         />
       </div>
