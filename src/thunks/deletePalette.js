9b4739be019d3a2ffd0b19f1605d315657cf0f53
Whitney Burton <whitneyburton9@gmail.com>
2019-03-26 17:41:11 -0600

    Refactor deletePalette thunk
    Co-authored-by: Hillary Stewart <hillarystewart4427@gmail.com>

--- src/thunks/deletePalette.js
+++ src/thunks/deletePalette.js
@@ -6,12 +6,10 @@ export const deletePalette = (id, palettes) => {
     const url = process.env.REACT_APP_BACKEND_URL + `/api/v1/palettes/${id}`;
     try {
       dispatch(setLoading(true));
-      const response = await deleteData(url);
-      const result = await response.json();
+      await deleteData(url);
       const updatedPalettes = palettes.filter(palette => {
         return palette.id !== id;
       });
-      console.log(updatedPalettes);
       dispatch(setLoading(false));
       dispatch(setPalettes(updatedPalettes));
     } catch (error) {
